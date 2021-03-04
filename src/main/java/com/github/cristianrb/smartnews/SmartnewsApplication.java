857111335e17b925e1f84a3e5ffafd23c75b0654
Cristian Ruiz <cristianrb235@gmail.com>
2021-03-04 16:08:07 +0100

    Project initialization

---
+++ src/main/java/com/github/cristianrb/smartnews/SmartnewsApplication.java
@@ -0,0 +1,13 @@
+package com.github.cristianrb.smartnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartnewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartnewsApplication.class, args);
+	}
+
+}
