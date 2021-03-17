a0fbfc19e9dd67a8d7e19138122f80b5f3abc924
Cristian Ruiz <cristianrb235@gmail.com>
2021-03-17 22:56:44 +0100

    Added token authentication

--- src/main/java/com/github/cristianrb/smartnews/SmartnewsApplication.java
+++ src/main/java/com/github/cristianrb/smartnews/SmartnewsApplication.java
@@ -1,10 +1,16 @@
 package com.github.cristianrb.smartnews;
 
-import com.github.cristianrb.smartnews.rss.RSSDownloader;
+import com.github.cristianrb.smartnews.security.JWTAuthorizationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @SpringBootApplication
 @EnableScheduling
@@ -13,6 +19,20 @@ public class SmartnewsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartnewsApplication.class, args);
+	}
+
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+					.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+					.authorizeRequests()
+					.antMatchers(HttpMethod.POST, "/user").permitAll()
+					.anyRequest().authenticated();
+		}
 	}
 
 }
