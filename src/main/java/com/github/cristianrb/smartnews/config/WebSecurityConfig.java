5d4ffe7379468ddd742ffe0cae3686d20ee6fe35
Cristian Ruiz <cristianrb235@gmail.com>
2021-04-12 23:58:02 +0200

    Changed cors config

--- src/main/java/com/github/cristianrb/smartnews/config/WebSecurityConfig.java
+++ src/main/java/com/github/cristianrb/smartnews/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.github.cristianrb.smartnews.config;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,19 +9,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @EnableWebSecurity
 @Configuration
+@EnableOAuth2Sso
 class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.cors().and().csrf().disable()
                 .antMatcher("/**").authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/api/latest", "/api/contributions").permitAll()
+                .antMatchers("/login", "/api/latest", "/api/contributions").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .oauth2Login()
-                .and()
                 .logout()
-                .logoutUrl("/logout")
                 .logoutSuccessUrl("/api/latest")
                 .permitAll();
     }
